d467f852adc9adb199c8a308cd3c67930012d797
troore <troore@gmail.com>
2016-01-01 23:17:27 +0800

    [MNT] modify prof_all.sh

--- scripts/prof_all.sh
+++ scripts/prof_all.sh
@@ -9,18 +9,20 @@ for dir in $hlsdirs
 do
 
 cd $dir
+benchdir=$PWD
 
 flist=`ls | grep "[a-zA-Z0-9_]*_prof_[a-zA-Z0-9]*_$1_[a-zA-Z0-9]*.tcl"`
 for f in $flist
 do
+echo $f
 vivado_hls -f $f
 prjdir=`echo $f | sed -e 's/.tcl/_prj/'`
-echo $prjdir
+#echo $prjdir
 cd $prjdir/solution1/impl/verilog
 sed -i '62 areport_power -file \$outputDir/\${outputBaseName}_power.rpt' run_vivado.tcl
 ./impl.sh
 
-cd $dir
+cd $benchdir
 done
 
 cd $rootdir
